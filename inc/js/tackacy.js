1f54e062194cb6a438a4c7e5038ed0c6ebc21996
Ooz47 <yannick.abaul@gmail.com>
2021-02-07 17:39:57 -0400

    inclusion tackacy.js

---
+++ inc/js/tackacy.js
@@ -0,0 +1,13 @@
+tarteaucitron.init({
+    "hashtag": "#tarteaucitron", /* Ouverture automatique du panel avec le hashtag */
+    "cookieName": "prefcookies", /* Cookie name */
+    "highPrivacy": true, /* désactiver le consentement implicite (en naviguant) ? */
+    "orientation": "bottom", /* le bandeau doit être en haut (top) ou en bas (bottom) ? */
+    "adblocker": false, /* Afficher un message si un adblocker est détecté */
+    "showAlertSmall": false, /* afficher le petit bandeau en bas à droite ? */
+    "cookieslist": false, /* Afficher la liste des cookies installés ? */
+    "removeCredit": false, /* supprimer le lien vers la source ? */
+    "moreInfoLink": false,
+    "handleBrowserDNTRequest": false, /* Deny everything if DNT is on */
+    //"cookieDomain": ".example.com" /* Nom de domaine sur lequel sera posé le cookie pour les sous-domaines */
+});
